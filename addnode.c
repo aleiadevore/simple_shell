2f290431b4ec9013750be4261bcae1b644399615
Aleia DeVore <2496@holbertonschool.com>
2020-11-14 02:50:14 +0000

    Updated to add node at end

--- addnode.c
+++ addnode.c
@@ -26,3 +26,37 @@ list_t *add_node(list_t **head, const char *str)
 	*head = newnode;
 	return (*head);
 }
+
+/**
+ * add_node_end - adds node to end of list
+ * @head: start of list
+ * @str: string
+ * Return: pointer to node or NULL
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *lastnode, *last = *head;
+	int count;
+
+	lastnode = malloc(sizeof(list_t));
+	if (!lastnode)
+	{
+		free(lastnode);
+		return (NULL);
+	}
+	lastnode->str = strdup(str);
+	for (count = 0; str[count] != '\0'; count++)
+		;
+	lastnode->len = count;
+	lastnode->next = NULL;
+	if (*head == NULL)
+	{
+		*head = lastnode;
+		return (lastnode);
+	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = lastnode;
+	return (lastnode);
+}
