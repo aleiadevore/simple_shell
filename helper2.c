107a87fd5413eb453582fe86a91c0dedb3501c27
CSant04y <esquivelcarlos27893@gmail.com>
2020-11-14 03:42:53 +0000

    added strncpy to the file

--- helper2.c
+++ helper2.c
@@ -39,3 +39,20 @@ char *_strcat(char *dest, char *src)
 
 	return (dest);
 }
+/**
+ * _strncpy - This copes up to n amount of bytes
+ * @dest: this is a pointer to the destination
+ * @src: this is a pointer to the src
+ * @n: This is the ammount of bytes to copy up to
+ * Return: is a pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
+	return (dest);
+}
