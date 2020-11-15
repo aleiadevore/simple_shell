6ff6c0848a18e5bad3e3853f13e47cc2c1bb49ce
CSant04y <esquivelcarlos27893@gmail.com>
2020-11-15 02:47:19 +0000

    added chnaged to token.c

--- token.c
+++ token.c
@@ -2,34 +2,47 @@
 /**
  * token - tokenizes user input
  * @input: input from terminal
- * @sz_input: size of user input from terminal
  * @head: head node of linked list
  */
 extern char **environ;
 list_t *token(char *input, size_t sz_input, list_t *head)
 {
 	char *tokenbuf;
+/*remove code sz_input And from prototype we do no need it to malloc anymore*/
+	(void)sz_input;
 
-	printf("Entering token function\n");
-	tokenbuf = malloc(sizeof(char) * (sz_input));
-	if (!tokenbuf)
+	/*tokenbuf = malloc(sizeof(char) * 40);*/
+
+/*	if (!tokenbuf)
 	{
 		printf("Failed to malloc token\n");
 		free(input);
 		exit(1);
 	}
-
+*/
 	tokenbuf = strtok(input, " ");
+	if (!tokenbuf)
+	{
+		free(input);
+		/*free(tokenbuf);*/
+		exit(4);
+	}
 	printf("Tokenbuff: %s\n", tokenbuf);
-	add_node_end(&head, input);
+	add_node_end(&head, tokenbuf);
 
 	while (tokenbuf != NULL)
 	{
+		print_list(head);
 		tokenbuf = strtok(NULL, " ");
+		if (!tokenbuf)
+			break;
+		add_node_end(&head, tokenbuf);
 		printf("%s\n", tokenbuf);
-		add_node_end(&head, input);
 	}
+
 	printf("Left while loop\n");
-	free(tokenbuf);
+	free(input);
+
+	/*free(tokenbuf);*/
 	return (head);
 }
