6427d34b30db7ec75bb739e5b1233a41f6c4d931
CSant04y <esquivelcarlos27893@gmail.com>
2020-11-15 02:46:56 +0000

    added changed to free statments

--- shell.c
+++ shell.c
@@ -10,21 +10,29 @@ int main(void)
 	list_t *head = NULL, *itr = NULL;
         char *b = NULL;
         size_t bufsize;
-        int characters;
+        int characters = 0;
 
 	while (1)
 	{
+		head = NULL;
+		b = NULL;
+	       	/*handels the ctrl - d option*/
 		if (isatty(STDIN_FILENO) == 1)
 			write(STDOUT_FILENO, "$ ", 2);
 
 		characters = getline(&b, &bufsize, stdin);
+
+		b = strtok(b, "\n");
+
 		if (characters == EOF)
 		{
 			write(STDOUT_FILENO, "EOF\n", 4);
+			free(b);
 			exit(0);
 		}
 
 		head = token(b, characters, head);
+
 		if (!head)
 		{
 			printf("Failed to create head node\n");
@@ -32,13 +40,14 @@ int main(void)
 		}
 /*		getenviron(b, head);*/
 		itr = head;
-		while (itr->token != NULL)
+		printf("itr = [%s]", itr->token);
+		while (itr != NULL)
 		{
-			printf("This is token buff %s", itr->token);
+
+			printf("This is token buff %s\n", itr->token);
 			itr = itr->next;
 		}
-		free(b);
-		free_list(head);
+		free_list(&head);
 	}
         return (0);
 }
