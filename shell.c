7a81f45aaca8ff94dbd17e3908d98a0002bb3097
Aleia DeVore <2496@holbertonschool.com>
2020-11-21 18:32:10 +0000

    Accounted for comments

--- shell.c
+++ shell.c
@@ -11,12 +11,13 @@ int main(void)
 	list_t *head = NULL;
 	char *b = NULL, n = '\0';
 	size_t bufsize;
-	int characters = 0, i = 0, bool = 0;
+	int characters = 0, i = 0, bool = 0, comment = 0;
 
 	while (1)
 	{
 		head = NULL;
 		b = NULL;
+		comment = 0;
 		if (isatty(STDIN_FILENO) == 1)
 			write(STDOUT_FILENO, "$ ", 2);
 		characters = getline(&b, &bufsize, stdin);
@@ -32,15 +33,16 @@ int main(void)
 				b[i] = n;
 		}
 		head = token(b, head);
-
 		if (!head)
 		{
 			perror("Failed to create head node\n");
 			free(b);
 		}
+		if (head->token[0] == '#')
+			comment = 1;
 		bool = envfunction(head);
-		/*bool = checkfunction(head);*/
-		if (bool != 1)
+		checkfunction(head);
+		if (bool != 1 && comment != 1)
 			dirsearch(head);
 		free_list(&head);
 	}
